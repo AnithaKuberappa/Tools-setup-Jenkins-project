5e6f57269a084adb485b1a8f8cec60690c07dcd2
AnithaKuberappa <66734687+AnithaKuberappa@users.noreply.github.com>
2024-04-04 12:48:28 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,26 @@
+After installing the Jenkins we need to install docker on Ubuntu machine using below setps 
 
+Docker installation On ubuntu Machine
+
+sudo apt udpate 
+
+sudo apt install docker.io 
+
+sudo systemctl enable docker
+
+sudo systemctl status docker
+
+After that install SonarQube using below docker command 
+
+ SonarQube:
+
+docker run -d --name sonar -p 9000:9000 sonarqube
+
+check docker ps command for checking the sonarQube 
+
+Ones SonarQube running add the inbound rules on Security group port 9000 range 00000
+
+http://13.232.157.12:9000
+
+Username: admin
+Password: admin
