6f814d6bf8a44fb7c982431d3a97f21169a86014
AnithaKuberappa <66734687+AnithaKuberappa@users.noreply.github.com>
2024-04-04 10:59:20 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -7,6 +7,25 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
+
+Jenkins Installation setps are followed:
+
+sudo apt-get update -y 
 sudo apt-get install jenkins -y
+
+Checking the status of jenkins and starting the jenkins server
+
 sudo systemctl start jenkins
+
 sudo systemctl status jenkins
+
+Set inbound rule on ec2 instance go to security group select inbound rule  tcp 8080 0.0.0.0.0/0
+
+Sigin Into Jenkins Console:
+
+http://3.107.39.180:8080
+
+
+SonarQube Installation setps
+
+docker run -d --name sonar -p 9000:9000 sonarqube
